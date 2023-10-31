87afd88537779a14574c46b0c0dac5e1a3374c8a
Elijah Ochoa <elijahochoa8106@gmail.com>
2023-10-31 03:28:44 -0500

    Initial Commit

---
+++ WorkUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkUp
+//
+//  Created by Elijah Ochoa on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkUp/WorkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkUpApp.swift
+//  WorkUp
+//
+//  Created by Elijah Ochoa on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
