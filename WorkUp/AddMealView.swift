f9e13cb0e8a9254e9560f886a5a271ac39fe6eda
Elijah Ochoa <elijahochoa8106@gmail.com>
2023-11-30 13:41:06 -0600

    Fixed Front end Code

---
+++ WorkUp/AddMealView.swift
@@ -0,0 +1,8 @@
+//
+//  AddMealView.swift
+//  WorkUp
+//
+//  Created by Elijah Ochoa on 11/30/23.
+//
+
+import Foundation
---
+++ WorkUp/CalorieTrackerView.swift
@@ -0,0 +1,8 @@
+//
+//  CalorieTrackerView.swift
+//  WorkUp
+//
+//  Created by Elijah Ochoa on 11/30/23.
+//
+
+import Foundation
